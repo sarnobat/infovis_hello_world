b0bf7d839ef380aff46f947ec24ac89fe1be27b2
sarnobat <ss401533@gmail.com>
2013-10-01 09:41:08 -0700

    Initial commit

--- example_2_using_server/run.sh
+++ example_2_using_server/run.sh
@@ -5,7 +5,13 @@ set -m
 mkdir -p ~/.groovy/lib
 cp ../.groovy/lib/*jar ~/.groovy/lib
 
+#Doesn't have CORS
+#python -m SimpleHTTPServer 8099
+#Can't get this to work on Mountain Lion. Need to run in Eclipse.
 groovy server.groovy &
+
+# 100 nodes is too big, infovis hangs
+
 sleep 3s
 
 open "index.html"
