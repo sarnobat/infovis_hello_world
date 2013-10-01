5d9fe1b369bc4672a0c62fb32835e335863f6fcf
sarnobat <ss401533@gmail.com>
2013-10-01 09:41:08 -0700

    Initial commit

--- example_2_using_server/server.groovy
+++ example_2_using_server/server.groovy
@@ -24,7 +24,7 @@ public class Server {
                 	// TODO: @QueryParam("rootId") Integer iRootId
                 	) throws JSONException {
                 		System.out.println("1");
-                        JSONObject json = new JSONObject();
+                        JSONObject json = new JSONObject("{				'id'			: 	'graphnode16','data'			:	{										'$color'		:	'#C74243',										'$type'			:	'star',										'$dim'			: 	17									},				'adjacencies'	:	[										{											'nodeTo'	:	'graphnode17',										},										{											'nodeTo'	:	'graphnode18',										}],},{	'id'			: 	'graphnode18',	'data'			:	{										'$color'		:	'blue',										'$type'			:	'square',										'$dim'			: 	7									},			}		]");
                 		System.out.println("2");
                         json.put("foo", "bar");
                 		System.out.println("3");
